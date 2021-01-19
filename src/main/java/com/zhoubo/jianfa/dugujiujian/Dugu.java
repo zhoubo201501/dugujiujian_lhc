6310a52f62405786624ff39adbe2ae6fb875ace6
zhoubo2014 <nadech2014@126.com>
2021-01-19 16:40:52 +0800

    岳不群修改

--- src/main/java/com/zhoubo/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/zhoubo/jianfa/dugujiujian/Dugu.java
@@ -3,5 +3,10 @@ package com.zhoubo.jianfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("jianfa dugujiujian!!!!!");
+        System.out.println("jianfa dugujiujian!!!!!");
+        System.out.println("jianfa dugujiujian!!!!!");
+        System.out.println("jianfa dugujiujian!!!!!");
+        System.out.println("jianfa dugujiujian!!!!!");
+
     }
 }
